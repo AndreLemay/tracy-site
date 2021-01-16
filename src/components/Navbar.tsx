f0da1fff3875cb61d53029092a9c82f372f7bba4
Andre Lemay <lemay.andre@gmail.com>
2021-01-15 19:57:48 -0500

    Add navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import React from "react"
+import { Link } from "gatsby"
+
+export default () => {
+    return (
+        <div>
+            <Link to="/">Home</Link>
+            <Link to="/">Tracy</Link>
+            <Link to="/">Books</Link>
+            <Link to="/">Sets/Props</Link>
+            <Link to="/">Visual Arts</Link>
+            <Link to="/">Crafts</Link>
+            <Link to="/">Blog</Link>
+            <Link to="/">Contact</Link>
+            <Link to="/">Upcoming Events</Link>
+        </div>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,45 +1,34 @@
 import React from "react"
-import { graphql, Link } from "gatsby"
+//import { graphql } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from "../components/Layout"
+import SEO from "../components/Seo"
 
-const IndexPage = ({ data }) => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      {data.allFile.edges.map(({ node }, idx) => {
-        return <Image key={idx} fluid={node.childImageSharp.fluid} />
-      })}
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
+const IndexPage = (/*{ data }*/) => (
+    <Layout>
+        <SEO title="Home" />
+    </Layout>
 )
 
 export default IndexPage
 
-export const query = graphql`
-  query {
-    allFile(
-      filter: {
-        extension: { regex: "/(jpg)|(png)|(tif)|(tiff)|(webp)|(jpeg)/" }
-        sourceInstanceName: { eq: "images" }
-      }
-    ) {
-      edges {
-        node {
-          childImageSharp {
-            fluid(maxWidth: 1920, quality: 80) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-      }
-    }
-  }
-`
+// export const query = graphql`
+//     query {
+//         allFile(
+//             filter: {
+//                 extension: { regex: "/(jpg)|(png)|(tif)|(tiff)|(webp)|(jpeg)/" }
+//                 sourceInstanceName: { eq: "images" }
+//             }
+//         ) {
+//             edges {
+//                 node {
+//                     childImageSharp {
+//                         fluid(maxWidth: 1920, quality: 80) {
+//                             ...GatsbyImageSharpFluid
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// `
