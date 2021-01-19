8272616e519d60d4ecd8f45bc8840e1fe8bd8f79
Andre Lemay <lemay.andre@gmail.com>
2021-01-18 19:53:24 -0500

    fix cols calculation

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -6,14 +6,18 @@ import Path from "path"
 
 interface ImageGalleryProps {
     images: FluidObject[]
-    cols?: GridSize
+    cols?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12
     spacing?: GridSpacing
 }
 export default ({ images, cols = 3, spacing = 2 }: ImageGalleryProps) => {
+    //have to do this as GridSize is just a type union of 1-12 plus "auto", and TS will complain if I try to give the result of a calculation directly to GridSize props
+    //i can be certain 12 / cols will always give m a value from 1-12 (aka valid GridSize) as I can't pass in anything but 1-12
+    const numCol = (12 / cols) as GridSize
+
     return (
-        <Grid container spacing={spacing} xs={12}>
+        <Grid container spacing={spacing}>
             {images.map((img, i) => (
-                <Grid key={i} xs={cols} item>
+                <Grid key={i} xs={numCol} item>
                     <Image fluid={img} alt={Path.basename(img.src)} />
                 </Grid>
             ))}
