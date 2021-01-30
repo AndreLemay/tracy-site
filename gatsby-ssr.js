6c198e2476975616a33806da6acaa9107f7775e9
Andre Lemay <lemay.andre@gmail.com>
2021-01-30 09:57:43 -0500

    fix ordering of meta tags in head (on build only)

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,15 @@
  */
 
 // You can delete this file if you're not using it
+export const onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
+    const headComponents = getHeadComponents()
+    const order = ["title", "base", "meta", "link", "noscript", "script", "style"]
+    const sortedHeadComponents = headComponents
+        .slice(0)
+        .flat()
+        .sort((x, y) => {
+            return order.indexOf(x.type) - order.indexOf(y.type)
+        })
+
+    replaceHeadComponents(sortedHeadComponents)
+}
