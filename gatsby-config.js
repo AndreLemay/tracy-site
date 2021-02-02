705fde7072aeee28394fb15c7b5e525eb1be5a5d
Andre Lemay <lemay.andre@gmail.com>
2021-02-02 15:20:57 -0500

    include protocol in netlify url

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
         description: `Portfolio/Author/Dabblings/Whatever.`,
         keywords: `author,lgbt,lgbtq+,book,`,
         twitterHandle: `@tracy_goldfarb`,
-        siteUrl: `tracy-site.netlify.app`,
+        siteUrl: `https://tracy-site.netlify.app`,
     },
     plugins: [
         // {
